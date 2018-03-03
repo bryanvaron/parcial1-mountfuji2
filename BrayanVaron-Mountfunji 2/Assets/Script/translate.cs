09ed4831affd849478cd23fa86319891644f065f
bryanvaron <bryanvaron@github>
2018-03-03 18:35:45 -0500

    bryanvaron
    monte fuji

---
+++ BrayanVaron-Mountfunji 2/Assets/Script/translate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class translate: MonoBehaviour
+{
+	public float movementSpeed = 10;
+
+	void Update(){
+
+		transform.Translate(Vector3.forward * movementSpeed * Time.deltaTime);
+
+	}
+}
